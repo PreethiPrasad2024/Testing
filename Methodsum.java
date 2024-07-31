c0df05d4573d1578e1a193995e5b26c31bf28aa5
PreethiPrasad2024 <spreethiprasad09@gmail.com>
2024-07-31 21:12:09 +0530

    Add files via upload

---
+++ Methodsum.java
@@ -0,0 +1,12 @@
+class Methodsum{
+
+public void sum(int a,int b){
+int sum=a+b;
+System.out.println("Sum of numbers="+sum);
+}
+public static void main(String[]args){
+
+Methodsum s=new Methodsum();
+s.sum(100,200);
+}
+}
